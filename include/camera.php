000bca0d5886e75df8aa7658654200fe9c8867f5
Marvin <iki@marvin.com>
2015-07-17 15:55:05 +0200

    Click-event on object circles

--- include/camera.php
+++ include/camera.php
@@ -1,4 +1,21 @@
 <script type="text/javascript" type="text/javascript">
+  var projectionMatrix = [];
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  var elements = [];
+  canvas.addEventListener('click', function(event) {
+    var canvasPos = canvas.getBoundingClientRect();
+    
+    var x = event.pageX - canvasPos.left,
+        y = event.pageY - canvasPos.top;
+    elements.forEach(function(element) {
+        var euclidDistance = Math.sqrt(Math.pow(x - element.coordinates[0], 2) + Math.pow(y - element.coordinates[1], 2));
+        if (euclidDistance <= element.radius) {
+            console.log('clicked element: ' + element.name);
+        }
+    });
+  }, false);
+
 	function projectPoint(point, projMatrix) {	// projMatrix is a 12 element vector representing the 3x4 matrix, point is of type geometry_msgs/Point
 		u = projMatrix[0] * point.x + projMatrix[1] * point.y + projMatrix[2] * point.z + projMatrix[3];
     v = projMatrix[4] * point.x + projMatrix[5] * point.y + projMatrix[6] * point.z + projMatrix[7];
@@ -9,8 +26,7 @@
 
     return [x, y];
 	}
-
-  var projectionMatrix = [];
+  
   var cameraInfoListener = new ROSLIB.Topic({
     ros : ros,
     name : '/camera/rgb/camera_info',
@@ -29,9 +45,10 @@
     cameraInfoListener.unsubscribe();
 
     objectListener.subscribe(function(message) {
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
+    
+    // clear everything
     context.clearRect(0, 0, canvas.width, canvas.height);
+    elements = [];
 
     var i;
     console.log('lenght: ' + message.objects.length);
@@ -43,7 +60,11 @@
         if (projectionMatrix.length == 12) {
           var coords = projectPoint(message.objects[i].pose.pose.pose.position, projectionMatrix);
           console.log('coords: ' + coords);
-
+          elements.push({
+            name: 'tbd',
+            coordinates: coords,
+            radius: 30
+          });
           var centerX = coords[0];
           var centerY = coords[1];
           var radius = 30;
@@ -67,8 +88,8 @@
   });
 </script>
 
-<div>
+<div id="parentDiv">
 <div style="position: absolute; z-index:100"><img src="http://192.168.5.2:8080/stream?topic=/camera/rgb/image_rect_color&type=mjpeg"></img></div>
-<div style="position: absolute; z-index:5000"><canvas id="myCanvas" width="640" height="480"></canvas></div>
+<div style="position: absolute; z-index:5000" id="canvasDiv"><canvas id="myCanvas" width="640" height="480"></canvas></div>
 
 </div>
