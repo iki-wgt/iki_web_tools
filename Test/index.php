951b528b508ab216477e4123c64d4003d89c8ee0
Ali <arasul@Macintosh.lan>
2015-09-29 12:48:31 +0200

    test interface push

--- Test/index.php
+++ Test/index.php
@@ -22,6 +22,7 @@
 		<link rel="stylesheet" type="text/css" href="css/icons.css" />
 		<link rel="stylesheet" type="text/css" href="css/component.css" />
 		<link rel="stylesheet" type="text/css" href="css/iki_robot.css" />
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
 		
 		
 		<script src="js/modernizr.custom.js"></script>
